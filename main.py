5a9c5b502755f89175e5f4065d87aa05b7557345
raghvendragkmit <raghvendra@gkmit.co>
2022-10-03 16:36:28 +0530

    add feature is added

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
+def add():
+    inp1 = int(input("Enter first number -> "))
+    inp2 = int(input("Enter second number -> "))
+    result = inp1 + inp2
+    print("Result of ",inp1,"+",inp2,'=', result)
 
+add()
