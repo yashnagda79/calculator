72b67520fc7e3f1ba0c99335e06464003b5f3632
Yuvi <apple@apples-MacBook-Pro.local>
2022-10-03 17:51:41 +0530

    division commited

--- main.py
+++ main.py
@@ -14,6 +14,15 @@ def add():
     inp2 = int(input("Enter second number -> "))
     result = inp1 + inp2
     print("Result of ",inp1,"+",inp2,'=', result)
+def div(a,b):
+    divide=a/b
+    return divide
+
+
+num1=int(input("input the number 1 : "))
+num2=int(input("input the number 2 : ")) 
+
+print("The result is ",div(num1,num2))
 
 
 add()
